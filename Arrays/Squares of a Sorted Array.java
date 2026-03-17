70e5055131e4d215e12f599788935c4c201866f2
jaivik patel <jaivikpatel2006@gmail.com>
2026-03-17 21:47:42 +0530

    Create Squares of a Sorted Array.java

---
+++ Arrays/Squares of a Sorted Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        
+        int left = 0, right = n - 1, idx = n - 1;
+        
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                res[idx--] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[idx--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        
+        return res;
+    }
+}
